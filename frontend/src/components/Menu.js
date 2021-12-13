86bd312a76b1e4b219f7968776cf0edd682c5cc6
jpcaponetto <86632183+jpcaponetto@users.noreply.github.com>
2021-12-13 18:19:03 -0300

    Ejemplo de card para Menu.

--- frontend/src/components/Menu.js
+++ frontend/src/components/Menu.js
@@ -1,10 +1,29 @@
 import React from "react";
-
+import hamburguesa from "../components/Menu";
 const Menu = () => {
   return (
-    <div>
-      <p> Menu </p>
-    </div>
+    <>
+      <div className="container">
+        <div className="row">
+          <div className="card-wrapper col-lg-4 col-md-6 col-xs-12">
+            <div className="card">
+              <div className="card-img-efecto card-img-top">
+                <img src={hamburguesa} />
+              </div>
+              <div className="card-body">
+                <h5 className="card-title"> Hamburguesa con Papas </h5>
+                <div className="card-content">
+                  <p className="texto-maridaje">
+                    Probá nuestras deliciosas hamburguesas Gourmet acompañadas
+                    de unas crujientes papas fritas
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
