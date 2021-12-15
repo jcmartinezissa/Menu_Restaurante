42e43d49ab4f5832fe1843f7497a2f359a7fbc23
jpcaponetto <86632183+jpcaponetto@users.noreply.github.com>
2021-12-14 22:15:23 -0300

    tarjetas genericas creadas con mockapi

--- frontend/src/Tarjeta/Tarjeta.js
+++ frontend/src/Tarjeta/Tarjeta.js
@@ -1,20 +1,22 @@
-import './Tarjeta.css';
+import "./Tarjeta.css";
 
-function Tarjeta(titulo, detalle, img, precio) {
-    return (
-        <>
-            <div class="card Tarjeta" style="width: 18rem;">
-                <img src={img} class="card-img-top" alt="..." />
-                <div class="card-body">
-                    <h5 class="card-title">{titulo}</h5>
-                    <p class="card-text">{detalle}</p>
-                    <p class="card-text">{precio}</p>
-                    <a href="#" class="btn btn-primary">Añadir</a>
-                </div>
-            </div>
-        </>
-
-    );
+function Tarjeta(props) {
+  const { img, titulo, detalle, precio } = props;
+  return (
+    <>
+      <div class="card Tarjeta" style={{ width: "18rem" }}>
+        <img src={img} class="card-img-top" alt="..." />
+        <div class="card-body">
+          <h5 class="card-title">{titulo}</h5>
+          <p class="card-text">{detalle}</p>
+          <p class="card-text">{precio}</p>
+          <a href="#" class="btn btn-primary">
+            Añadir
+          </a>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Tarjeta;
